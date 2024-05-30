62e17dad65157f163bf7ea3d763cfcc8334f6272
Afnan Mojumder <95128657+afnan-mojumder@users.noreply.github.com>
2024-05-30 12:42:50 +0600

    main

---
+++ src/BeginnerJava/IncrementDecrement.java
@@ -0,0 +1,15 @@
+package BeginnerJava;
+
+public class IncrementDecrement {
+
+    public static void main(String[] args){
+        int x = 25;
+        int y;
+
+        y = x++;
+        y = x;
+        
+        System.out.println("y = " + y);
+    }
+
+}
--- src/BeginnerJava/Temperature.java
+++ src/BeginnerJava/Temperature.java
@@ -1,1 +1,17 @@
+package BeginnerJava;
+import java.util.Scanner;
+public class Temperature {
 
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        double cels, farn;
+        System.out.print("Celsius = ");
+        cels = input.nextDouble();
+        farn = 1.8 * cels + 32;
+
+        System.out.println("Fahrenheit: " + farn);
+
+    }
+
+}
