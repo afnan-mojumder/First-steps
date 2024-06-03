f3e5b60c3b804f6df0f7506fbe7ab1137dcc9e32
Afnan Mojumder <95128657+afnan-mojumder@users.noreply.github.com>
2024-06-03 15:05:08 +0600

    June 3 practice session

---
+++ src/BeginnerJava/positiveJava.java
@@ -0,0 +1,25 @@
+
+package BeginnerJava;
+import java.util.Scanner;
+public class positiveJava {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter any integer: ");
+        num = input.nextInt();
+        if(num>0){
+            System.out.println("Positive");
+
+        }
+        else if(num<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+        
+        
+    }
+
+}
