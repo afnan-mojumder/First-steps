7b282725f09c4b0e399045cf1483be3df98566ca
Afnan Mojumder <95128657+afnan-mojumder@users.noreply.github.com>
2024-06-20 07:08:12 +0600

    morning practice

--- src/Switch101/Call_Center.java
+++ src/Switch101/Call_Center.java
@@ -2,6 +2,27 @@ package Switch101;
 import java.util.Scanner;
 public class Call_Center {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int option;
+        System.out.print("Enter any option: ");
+
+        option = input.nextInt();
+
+        switch(option){
+
+            case 1 :
+               System.out.println("Selected language is Bengali.");
+               break;
+            case 2 :
+               System.out.println("Selected language is Hindi.");
+               break;
+            case 3 :
+               System.out.println("Selected language is Urdu.");
+               break;
+            default :
+               System.out.println("Selected Language is English");
+
+        }
         
     }
 
